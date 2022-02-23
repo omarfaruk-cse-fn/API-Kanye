0c27bd3f5573f2495beb46f00f94adfadc0d2c52
Omar Faruk <omarfaruk.cse.fn@gmail.com>
2022-02-23 18:41:58 +0600

    Countries Details

--- js/countries.js
+++ js/countries.js
@@ -11,14 +11,26 @@ const displayCountries = (data) => {
     data.forEach(country => {
         const div = document.createElement('div')
         div.classList.add('country')
-        const h3 = document.createElement('h3')
-        h3.innerText = `Name: ${country.name.common}`
-        div.appendChild(h3)
-        const p = document.createElement('p')
-        p.innerText = `Official Name : ${country.name.official}
-        Capital : ${country.capital}`
-        div.appendChild(p)
+        div.innerHTML = `
+        <h3>Name: ${country.name.common}</h3>
+        <p>Official Name : ${country.name.official}</p >
+        <p> Capital : ${country.capital}</p >
+        <button onclick="loadCountryByName('${country.name.common}')"> Details </button>
+        `
         countriesDib.appendChild(div)
     })
-
+}
+const loadCountryByName = (name) => {
+    const url = `https://restcountries.com/v3.1/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]))
+}
+const displayCountryDetail = (country) => {
+    const countryDiv = document.getElementById('country-detail')
+    countryDiv.innerHTML = `
+    <h5>Country Name: ${country.name.common}</h5>
+    <p>Population: ${country.population}</p>
+    <img src="${country.name.flag}">
+    `
 }
